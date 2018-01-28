a8ffa883d66d8c3ea745a57bfac5b861f0638967
zub <crazysal.14@gmail.com>
2018-01-27 22:51:00 -0500

    get Nutrients

--- node/routes/index.js
+++ node/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,5 +12,30 @@ router.get('/calculator', function(req, res, next) {
 	// res.send({"test":"test"})
   res.render('calculator', { title: 'Express' });
 });
+
+
+
+getNutr = function(req,res,next){
+	urrrl = "https://api.nal.usda.gov/ndb/nutrients/?max=10&sort=c&fg=0100&fg=0500&format=json&api_key=DEMO_KEY&nutrients=203&nutrients=208&nutrients=204"
+	responseHandle =  (error, response, body) => {
+		if(error){
+			res.send(error)
+		}
+		res.send(body)
+	}
+
+
+
+	request.get(urrrl, responseHandle)
+	// res.send("muh me lele")
+}
+
+
+
+
+
+
+
+router.get('/nutr', getNutr);
 
 module.exports = router;
